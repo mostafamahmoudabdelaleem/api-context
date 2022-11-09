4e92cecb78b8b0f16eb5712cb688da293f9ded88
Mostafa Mahmoud <mostafa.mahmoud@it-fusion.org>
2022-11-09 16:48:16 +0200

    update example

--- example/Page.js
+++ example/Page.js
@@ -1,28 +1,42 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { APIContext } from 'react-native-api-context';
 
 export default function Page() {
-    const { Post } = React.useContext(APIContext)
+    const { Get } = React.useContext(APIContext)
     const [data, setData] = React.useState('')
 
     React.useEffect(() => {
-        Post({
+        Get({
             path: 'posts'
         })
             .then(resp => {
-                console.log('resp ===>', resp)
-                // setData(resp)
+                setData(resp)
             })
             .catch(err => {
-                console.log('error ===>', err)
+                console.log('[APIContext error]:', err)
             })
-            
+
     }, []);
 
     return (
         <View style={styles.container}>
-            <Text>Response: {data}</Text>
+            <FlatList
+                data={data}
+                renderItem={({ item, index }) => {
+                    return (
+                        <View key={index} style={styles.card}>
+                            <Text style={{ color: '#343434' }}>
+                                {`${item.title}`.length > 30 ? `${`${item.title}`.substring(0, 30)} ...` : `${item.title}`}
+                            </Text>
+
+                            <Text style={{ color: '#999', fontSize: 10 }}>
+                                {`${item.body}`.length > 150 ? `${`${item.body}`.substring(0, 150)} ...` : `${item.body}`}
+                            </Text>
+                        </View>
+                    )
+                }}
+            />
         </View>
     );
 }
@@ -30,7 +44,13 @@ export default function Page() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#f1f1f3'
+    },
+    card: {
+        margin: 8,
+        padding: 16,
+        backgroundColor: '#fff',
+        color: '#343434',
+        borderRadius: 8
     }
 });
